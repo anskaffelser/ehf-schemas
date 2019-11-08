85948d77c8b30729229e232fbddd34e11c2bd126
Updater <erlend@klakegg.net>
2019-11-08 23:01:15 +0100

    Initial support for aliases. #4

--- script/prepare_buildconfig.sh
+++ script/prepare_buildconfig.sh
@@ -1,19 +1,33 @@
 #!/bin/sh
 
-echo '<?xml version="1.0" encoding="UTF-8"?>'
-echo '<buildConfigurations xmlns="http://difi.no/xsd/vefa/validator/1.0">'
-echo '\t<configuration>'
-echo '\t\t<identifier>no.anskaffelser.ehf.schemas::all</identifier>'
-echo '\t\t<title>EHF Schemas</title>'
-echo '\t\t<file type="xml.xsd" path="xsd/_includes.xsd"/>'
-echo '\t</configuration>'
+set -e
+set -u
+
+identifier="ehf-schemas"
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+echo "<buildConfigurations xmlns=\"http://difi.no/xsd/vefa/validator/1.0\">"
+echo "\t<configuration>"
+echo "\t\t<identifier>${identifier}</identifier>"
+echo "\t\t<title>EHF Schemas</title>"
+echo "\t\t<file type=\"xml.xsd\" path=\"xsd/_includes.xsd\"/>"
+echo "\t</configuration>"
 
 echo
 
-for p in $(find xsd -type f -name '*.xsd' | sort); do
-  echo -n '\t<include path="'
-  echo -n $p
-  echo '" />'
+if [ -e aliases ]; then
+  for alias in $(cat aliases); do
+    echo "\t<configuration>"
+    echo "\t\t<identifier>${alias}</identifier>"
+    echo "\t\t<inherit>${identifier}</inherit>"
+    echo "\t</configuration>"
+  done
+fi
+
+echo
+
+for path in $(find xsd -type f -name '*.xsd' | sort); do
+  echo "\t<include path=\"$path\"/>"
 done
 
-echo '</buildConfigurations>'
+echo "</buildConfigurations>"
